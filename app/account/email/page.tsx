5b4d408b271f1bc939213af24782c22f5cdbc5f4
Leminhhieu06102311 <Leminhhieu06102003@gmail.com>
2023-07-20 11:58:47 +0700

    upload base source

---
+++ app/account/email/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return (
+        <h1>EMAIL</h1>
+    )
+}
---
+++ app/tasks/[IdTask]/page.tsx
@@ -0,0 +1,5 @@
+export default function Home({IdTask} : any) {
+    return (
+        <h1>{IdTask}</h1>
+    )
+}
